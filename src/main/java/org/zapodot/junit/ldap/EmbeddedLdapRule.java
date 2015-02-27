1ad8d1987ee33181a5ed421c6226778556b88537
Sondre Eikanger Kvalø <zapodot@gmail.com>
2015-02-27 15:55:02 +0100

    Added documentation to the public API and also specifies JDK source and target

--- src/main/java/org/zapodot/junit/ldap/EmbeddedLdapRule.java
+++ src/main/java/org/zapodot/junit/ldap/EmbeddedLdapRule.java
@@ -7,9 +7,24 @@ import org.junit.rules.TestRule;
 import javax.naming.NamingException;
 import javax.naming.directory.InitialDirContext;
 
+/**
+ * A JUnit rule that may be used as either a @Rule or a @ClassRule
+ */
 public interface EmbeddedLdapRule extends TestRule {
 
+    /**
+     * For tests depending on the UnboundID LDAP SDK
+     *
+     * @return a shared LDAPConnection
+     * @throws LDAPException i a connection can not be opened
+     */
     LDAPConnection ldapConnection() throws LDAPException;
 
+    /**
+     * For tests depending on the standard Java JNDI API
+     *
+     * @return a shared InitialDirContext
+     * @throws NamingException if a context can not be established
+     */
     InitialDirContext initialDirContext() throws NamingException;
 }
--- src/main/java/org/zapodot/junit/ldap/EmbeddedLdapRuleBuilder.java
+++ src/main/java/org/zapodot/junit/ldap/EmbeddedLdapRuleBuilder.java
@@ -11,6 +11,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * A builder providing a fluent way of defining EmbeddedLdapRule instances
+ */
 public class EmbeddedLdapRuleBuilder {
 
     public static final String DEFAULT_DOMAIN = "dc=example,dc=com";
@@ -32,37 +35,77 @@ public class EmbeddedLdapRuleBuilder {
     public EmbeddedLdapRuleBuilder() {
     }
 
+    /**
+     * Creates a new builder
+     *
+     * @return a new EmbeddedLdapRuleBuilder instance
+     */
     public static EmbeddedLdapRuleBuilder newInstance() {
         return new EmbeddedLdapRuleBuilder();
     }
 
+    /**
+     * Sets the domainDsn to be used. If not set, it will default to the value of the {@link #DEFAULT_DOMAIN DEFAULT_DOMAIN} field
+     *
+     * @param domainDsn a valid DSN string
+     * @return same EmbeddedLdapRuleBuilder instance with the domainDsn field set
+     */
     public EmbeddedLdapRuleBuilder usingDomainDsn(final String domainDsn) {
         this.domainDsn = domainDsn;
         return this;
     }
 
+    /**
+     * Sets the DSN to bind to when authenticating. If not set, it will default to the value of the {@link #DEFAULT_BIND_DSN DEFAULT_BIND_DSN} field
+     *
+     * @param bindDSN a valid DSN string
+     * @return same EmbeddedLdapRuleBuilder instance with the bindDSN field set
+     */
     public EmbeddedLdapRuleBuilder usingBindDSN(final String bindDSN) {
         this.bindDSN = bindDSN;
         return this;
     }
 
+    /**
+     * Sets the credentials to be used to authenticate. If not set, it will default to the value of the {@link #DEFAULT_BIND_CREDENTIALS DEFAULT_BIND_CREDENTIALS} field
+     *
+     * @param bindCredentials a password string
+     * @return same EmbeddedLdapRuleBuilder instance with the bindCredentials field set
+     */
     public EmbeddedLdapRuleBuilder usingBindCredentials(final String bindCredentials) {
         this.bindCredentials = bindCredentials;
         return this;
     }
 
+    /**
+     * Sets the port that the in-memory LDAP server will bind to. If not set, an available port will be picked automatically
+     *
+     * @param port a port number
+     * @return same EmbeddedLdapRuleBuilder instance with the port field set
+     */
     public EmbeddedLdapRuleBuilder bindingToPort(final int port) {
         this.port = Integer.valueOf(port);
         return this;
     }
 
+    /**
+     * Specify one or more LDIF resources to be imported on startup.
+     *
+     * @param ldifFiles LDIF-files to import
+     * @return same EmbeddedLdapRuleBuilder instance with the provided ldifFiles added to the list of LDIF files to import
+     */
     public EmbeddedLdapRuleBuilder importingLdifs(final String... ldifFiles) {
-        if(ldifFiles != null) {
+        if (ldifFiles != null) {
             ldifsToImport.addAll(Arrays.asList(ldifFiles));
         }
         return this;
     }
 
+    /**
+     * Creates a new rule based on the information that was previously provided
+     *
+     * @return
+     */
     public EmbeddedLdapRule build() {
         Objects.requireNonNull(bindDSN, "\"bindDSN\" can not be null");
         return EmbeddedLdapRuleImpl.createForConfiguration(createInMemoryServerConfiguration(),
@@ -75,24 +118,26 @@ public class EmbeddedLdapRuleBuilder {
             final InMemoryDirectoryServerConfig inMemoryDirectoryServerConfig =
                     new InMemoryDirectoryServerConfig(domainDsn);
 
-            if(bindCredentials != null) {
+            if (bindCredentials != null) {
                 this.authenticationConfiguration = new AuthenticationConfiguration(bindDSN, bindCredentials);
                 inMemoryDirectoryServerConfig.addAdditionalBindCredentials(bindDSN, bindCredentials);
             }
 
-            if(port != null) {
+            if (port != null) {
                 inMemoryDirectoryServerConfig.setListenerConfigs(InMemoryListenerConfig.createLDAPConfig(
                         LDAP_SERVER_LISTENER_NAME,
-                                                                                                         port));
+                        port));
             } else {
-                inMemoryDirectoryServerConfig.setListenerConfigs(InMemoryListenerConfig.createLDAPConfig(LDAP_SERVER_LISTENER_NAME));
+                inMemoryDirectoryServerConfig.setListenerConfigs(InMemoryListenerConfig.createLDAPConfig(
+                        LDAP_SERVER_LISTENER_NAME));
             }
             return inMemoryDirectoryServerConfig;
         } catch (LDAPException e) {
-            throw new IllegalStateException("Could not create configuration for the in-memory LDAP instance due to an exception", e);
+            throw new IllegalStateException(
+                    "Could not create configuration for the in-memory LDAP instance due to an exception",
+                    e);
         }
     }
-
 
 
 }
